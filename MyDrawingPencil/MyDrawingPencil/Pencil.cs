46df343acf7b857f8abcb717a2de3bf8815737f6
Constantin Hitov <kostya.khitov@mail.ru>
2019-05-02 10:28:42 +0300

    Create new plugin

---
+++ MyDrawingPencil/MyDrawingPencil/Pencil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using MyShape;
+
+namespace MyDrawingPencil
+{
+    public class Pencil : Shape
+    {
+        public Pencil(Color clr, int pWidth)
+        {
+            this.clr = clr;
+            this.pWidth = pWidth;
+        }
+
+        public override Bitmap Draw(Bitmap bmp, int x, int y, int h, int w, Point first, Point second)
+        {
+            Graphics graph = Graphics.FromImage(bmp);
+            graph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Pen pen = new Pen(clr);
+            pen.Width = pWidth;
+            graph.DrawLine(pen, first.X, first.Y, second.X, second.Y);
+            return bmp;
+        }
+
+        public override void DrawE(int x, int y, int h, int w, Point first, Point second, PaintEventArgs e)
+        {
+            Pen pen = new Pen(clr);
+            pen.Width = pWidth;
+            e.Graphics.DrawLine(pen, first.X, first.Y, second.X, second.Y);
+        }
+    }
+}
