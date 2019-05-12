56c755911b68cc48372ccb10a4b18566ad74c01e
Constantin Hitov <kostya.khitov@mail.ru>
2019-05-12 11:16:29 +0300

    Add ShapeFactory

---
+++ MyShape/MyShape/ShapeFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShape
+{
+    public abstract class ShapeFactory
+    {
+        public string Name { get; set; }
+        public abstract Shape Create();
+    }
+}
