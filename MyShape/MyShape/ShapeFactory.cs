91850956d623781eeef5c8048d43536e106d4bd5
Constantin Hitov <kostya.khitov@mail.ru>
2019-05-12 13:10:16 +0300

    Update ShapeFactory

--- MyShape/MyShape/ShapeFactory.cs
+++ MyShape/MyShape/ShapeFactory.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace MyShape
 {
     public abstract class ShapeFactory
     {
         public string Name { get; set; }
-        public abstract Shape Create();
+        public abstract Shape Create(Color clr, int pWidth);
     }
 }
