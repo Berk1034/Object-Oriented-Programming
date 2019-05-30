bf8fed2822cde6606eb13bbfa32f05d5f7fee5fc
Constantin Hitov <kostya.khitov@mail.ru>
2019-05-30 20:51:56 +0300

    Rework Abstract-Class Shape

--- MyShape/MyShape/Shape.cs
+++ MyShape/MyShape/Shape.cs
@@ -11,11 +11,10 @@ namespace MyShape
     public abstract class Shape
     {
         public Point pos1, pos2;
-        public int x, y, h, w;
         public Color clr;
         public int pWidth;
 
-        public abstract Bitmap Draw(Bitmap bmp, int x, int y, int h, int w, Point first, Point second);
-        public abstract void DrawE(int x, int y, int h, int w, Point first, Point second, PaintEventArgs e);
+        public abstract Bitmap Draw(Bitmap bmp, Point first, Point second);
+        public abstract void DrawE(Point first, Point second, PaintEventArgs e);
     }
 }
